f479235c775fca95a4760121168cb72bb52ba05d
Chris Bradford <chjabr@gmail.com>
2021-04-13 21:35:29 -0500

    Don't override settings when importing compendium scenes.

--- src/module/utils.js
+++ src/module/utils.js
@@ -4,6 +4,8 @@
  */
 export const modName = "scene-defaults"
 
+// Returns true if this scene is a copy of an existing one
+// I.e. is a duplicate or an import
 export function isCopy(data) {
-    return !!data._id;
+    return !data.type;
 }
